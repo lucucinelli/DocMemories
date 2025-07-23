1e79448306463e9f5efd1f04af168babd75d6d7c
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-07-23 13:05:52 +0200

    Update find.blade.php

--- resources/views/patients/find.blade.php
+++ resources/views/patients/find.blade.php
@@ -8,7 +8,7 @@
     
         <div class="py-12">
             <div class="hidden sm:block">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
                             <table class="min-w-full table-auto border-separate border-spacing-x-6 divide-y divide-gray-200 dark:divide-gray-700">
