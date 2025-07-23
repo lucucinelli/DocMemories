a961db81b12ddd4e84c98e26c92858c5c5da3eba
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-07-23 13:37:46 +0200

    Update patient table and mobile view dark mode styles
    Improved dark mode color consistency for table headers, rows, and action links in the patients list. Adjusted background and hover colors to enhance readability and maintain a uniform appearance across light and dark themes.

--- resources/views/patients/find.blade.php
+++ resources/views/patients/find.blade.php
@@ -9,38 +9,38 @@
         <div class="py-12">
             <div class="hidden sm:block">
                 <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
-                            <table class="min-w-full table-auto border-separate border-spacing-x-6 divide-y divide-gray-200 dark:divide-gray-700">
-                                <thead class="bg-gray-100 dark:bg-gray-700">
+                    <div class="bg-white dark:bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white dark:bg-gray-200 border-b border-gray-200 dark:border-gray-200">
+                            <table class="min-w-full table-auto dark:bg-gray-200">
+                                <thead  class="bg-gray-100 dark:bg-gray-700">
                                     <tr>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Nome') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Cognome') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Data di nascita') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Genere') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Telefono') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Email') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Visualizza') }}
                                         </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-gray-300 uppercase tracking-wider">
+                                        <th scope="col" class="px-6 py-3 text-left text-s font-medium text-black dark:text-white uppercase tracking-wider">
                                             {{ __('Elimina') }}
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                                <tbody class="bg-white dark:bg-gray-200 divide-y">
                                     @foreach($patients as $patient)
                                     <tr>
                                         <td class="px-6 py-4 whitespace-nowrap">
@@ -62,10 +62,10 @@
                                             {{ $patient->email }}
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                            <a href="{{ route('showPatient', $patient->id) }}" class="text-blue-600 hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-600">{{ __('Visualizza') }}</a>
+                                            <a href="{{ route('showPatient', $patient->id) }}" class="text-blue-600 hover:text-blue-900 dark:text-blue-600 dark:hover:text-blue-900">{{ __('Visualizza') }}</a>
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                            <a href="{{ route('deletePatient', $patient->id) }}" class="text-red-600 hover:text-gray-600 dark:text-red-600 dark:hover:text-white">{{ __('Elimina') }}</a>
+                                            <a href="{{ route('deletePatient', $patient->id) }}" class="text-red-600 hover:text-red-900 dark:text-red-600 dark:hover:text-red-900">{{ __('Elimina') }}</a>
                                         </td>
                                     </tr>
                                     @endforeach
@@ -78,15 +78,15 @@
             {{-- Mobile view --}}
             <div class= "block sm:hidden  mt-2 space-y-6">
                 @foreach($patients as $patient)
-                    <div class="ml-4 mr-4 bg-gray-200 dark:bg-gray-800 p-4 rounded-lg shadow-sm">
+                    <div class="ml-4 mr-4 bg-gray-200 dark:bg-gray-200 p-4 rounded-lg shadow-sm">
                         <h3 class="text-lg font-semibold">{{ $patient->name }} {{ $patient->surname }}</h3>
                         <p>{{ __('Data di nascita: ') . $patient->birthdate->format('d/m/Y') }}</p>
                         <p>{{ __('Genere: ') . ($patient->gender === 'M' ? 'Maschio' : 'Femmina') }}</p>
                         <p>{{ __('Telefono: ') . $patient->telephone }}</p>
                         <p>{{ __('Email: ') . $patient->email }}</p>
                         <div class="mt-4 flex space-x-4">
-                            <a href="{{ route('showPatient', $patient->id) }}" class="text-blue-600 hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-600">{{ __('Visualizza') }}</a>
-                            <a href="{{ route('deletePatient', $patient->id) }}" class="text-red-600 hover:text-gray-600 dark:text-red-600 dark:hover:text-white">{{ __('Elimina') }}</a>
+                            <a href="{{ route('showPatient', $patient->id) }}" class="text-blue-600 hover:text-blue-900 dark:text-blue-600 dark:hover:text-blue-900">{{ __('Visualizza') }}</a>
+                            <a href="{{ route('deletePatient', $patient->id) }}" class="text-red-600 hover:text-gray-600 dark:text-red-600 dark:hover:text-red-900">{{ __('Elimina') }}</a>
                         </div>
                     </div>
                 @endforeach
