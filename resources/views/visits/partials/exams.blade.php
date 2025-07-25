b15ea627a9a913dafc3e0246a8cb5f54f867d899
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-07-25 23:11:54 +0200

    Add flex layout to section headers in visit partials
    Updated the <h2> headers in exams, medicinals, and tests partials to use flexbox for improved alignment of section titles and icons.

--- resources/views/visits/partials/exams.blade.php
+++ resources/views/visits/partials/exams.blade.php
@@ -1,9 +1,8 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            <h2 class="flex justify-between items-center text-lg font-medium text-gray-900 dark:text-gray-100">
                 {{ __('Esami') }} <i class="bi bi-caret-down-fill" id="exams-toggle-Down"></i>
-
             </h2>
             <div id="exams-list" class="mt-4 hidden">
 
