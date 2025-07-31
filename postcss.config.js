a8d16ff36cc48a0be8bb10acd6dc0fcee08044e6
Ludovica Cucinelli <ludovica.cucinelli@student.univaq.it>
2025-07-31 16:20:18 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
+export default {  // plugins that are used by PostCSS in order to edit CSS files, according to this order
     plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+        tailwindcss: {},  // the brackets are empty because PostCSS will use the default Tailwind CSS options
+        autoprefixer: {}, // it adds vendor prefixes to CSS properties for better browser compatibility using Can I Use data
     },
 };
