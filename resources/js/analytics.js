0ac4a8da84564cf435bfece162aa46ecce63c7e8
3ndryw <andreacostantini03@gmail.com>
2025-08-06 22:45:46 +0200

    Update analytics.js

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -381,8 +381,8 @@ function createChartStepper(type, label, datasets, title){
                 label: 'Pazienti',
                 data: datasets,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 99, 132, 0.5)',
                     'rgba(255, 206, 86, 0.5)',
                     'rgba(75, 192, 192, 0.5)',
                     'rgba(153, 102, 255, 0.5)',
