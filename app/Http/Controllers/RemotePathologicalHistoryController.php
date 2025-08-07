c716f86bd628e37068fba9ed074284181a90aabb
3ndryw <andreacostantini03@gmail.com>
2025-08-07 21:06:24 +0200

    Update RemotePathologicalHistoryController.php

--- app/Http/Controllers/RemotePathologicalHistoryController.php
+++ app/Http/Controllers/RemotePathologicalHistoryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 use App\Models\RemotePathologicalHistory;
 use App\Exports\RemotePathologicalHistoryExport;
 
