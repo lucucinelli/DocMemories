ad9285af25afa5ea02dc991e0ae730452a8e58e9
3ndryw <andreacostantini03@gmail.com>
2025-08-08 19:18:14 +0200

    Add model factories for patient and visit entities
    Introduced new Eloquent model factories for Patient, Visit, AllergyTest, Exam, FamiliarHistory, Medicinal, NextPathologicalHistory, PhysiologicalHistory, and RemotePathologicalHistory. These factories will facilitate database seeding and testing by providing default data generation for each model.

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'surname' => $this->faker->lastName(),
+            'birthdate' => $this->faker->date(),
+            'gender' => $this->faker->randomElement(['M', 'F', 'Non specificato']),
+            'birthplace' => $this->faker->city(),
+            'tax_code' => $this->faker->unique()->regexify('^[A-Z]{6}[0-9]{2}[A-EHLMPR-T][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
+            'marital_status' => $this->faker->randomElement(['Single', 'Married', 'Divorced']),
+            'nationality' => $this->faker->country(),
+            'city' => $this->faker->city(),
+            'province' => $this->faker->state(),
+            'address' => $this->faker->streetAddress(),
+            'street_number' => $this->faker->buildingNumber(),
+            'zip_code' => $this->faker->regexify('^[0-9]{5}$'),
+            'domicile_city' => $this->faker->city(),
+            'domicile_province' => $this->faker->state(),
+            'domicile_address' => $this->faker->streetAddress(),
+            'domicile_street_number' => $this->faker->buildingNumber(),
+            'domicile_zip_code' => $this->faker->regexify('^[0-9]{5}$'),
+            'telephone' => $this->faker->unique()->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'occupation' => $this->faker->word(),
+        ];
+    }
+}
