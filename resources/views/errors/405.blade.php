e0d39a5c6871f0783a867c013578acfb5dd9263b
lucucinelli <ludovica.cucinelli@student.univaq.it>
2025-08-13 22:13:05 +0200

    Create 405.blade.php

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <main class="max-w-7xl mx-auto my-32 py-6 sm:px-6 lg:px-8 ">
+        <div class="text-center">
+            <p class="text-lg font-semibold text-red-600">405</p>
+            <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-gray-100 sm:text-7xl"> Metodo non consentito </h1>
+        </div>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="{{ route('dashboard') }}" class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Torna alla home</a>
+        </div>
+    </main>
+</x-app-layout>
