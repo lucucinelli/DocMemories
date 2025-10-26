212e7dae6def0faffcfe7cecc241681fff9ce554
Ludovica Cucinelli <ludovica.cucinelli@student.univaq.it>
2025-10-26 14:45:17 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <div class="backgroundImage" style="background-image: url('{{ asset('images/dna2.jpg') }}');"> </div>
+        <div class="backgroundImage" style="background-image: url('{{ asset('images/ap.jpg') }}');"> </div>
         <div class="z-10 flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0" >
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
